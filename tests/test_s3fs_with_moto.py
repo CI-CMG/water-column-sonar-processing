e42a153c6fc1be3a2c143e2d957402a79025b94a
oftfrfbf <rudyklucik@gmail.com>
2024-11-08 20:42:42 -0700

    simplified s3fs and boto3 testing

--- tests/test_s3fs_with_moto.py
+++ tests/test_s3fs_with_moto.py
@@ -12,12 +12,7 @@ from s3fs import S3FileSystem
 # from moto import mock_aws
 from moto.moto_server.threaded_moto_server import ThreadedMotoServer
 
-test_bucket = "test"
-files = [
-    "test/test.txt",
-    "test/subdir/test2.txt",
-    "test/subdir2/test3.txt",
-]
+test_bucket = "mybucket"
 ip_address = "127.0.0.1"
 port = 5555
 endpoint_url = f"http://{ip_address}:{port}"
@@ -26,79 +21,69 @@ endpoint_url = f"http://{ip_address}:{port}"
 def s3_base():
     s3_server = ThreadedMotoServer(ip_address=ip_address, port=port)
     s3_server.start()
-    if "AWS_ACCESS_KEY_ID" not in os.environ:
-        os.environ["AWS_ACCESS_KEY_ID"] = "test"
-    if "AWS_SECRET_ACCESS_KEY" not in os.environ:
-        os.environ["AWS_SECRET_ACCESS_KEY"] = "test"
+    # if "AWS_ACCESS_KEY_ID" not in os.environ:
+    #     os.environ["AWS_ACCESS_KEY_ID"] = "test"
+    # if "AWS_SECRET_ACCESS_KEY" not in os.environ:
+    #     os.environ["AWS_SECRET_ACCESS_KEY"] = "test"
     yield
     s3_server.stop()
 
 
-@pytest.fixture()
-def init_s3_files(s3_base):
+def test_load_all_files(s3_base):
+    s3_session = boto3.Session()
+    s3_client = s3_session.client(service_name="s3", endpoint_url=f"http://{ip_address}:{port}")
+    s3_client.list_buckets()
+
     s3fs = S3FileSystem(endpoint_url=endpoint_url)
-    s3fs.mkdir(test_bucket)
-    s3fs.mkdir(f"{test_bucket}/subdir")
-    s3fs.mkdir(f"{test_bucket}/subdir2")
-    s3fs.ls(f"{test_bucket}")
+
     with open("test.foo1", "w") as file:
         file.write("test123")
 
-    with open("test.bar", "w") as file:
+    with open("test.foo2", "w") as file:
         file.write("test456")
 
-    # Create a mock S3 bucket
-    #s3 = boto3.client("s3", region_name='us-east-1', endpoint_url="http://localhost:5000")
-    s3_session = boto3.Session() # aws_access_key_id=os.environ.get("ACCESS_KEY_ID"), aws_secret_access_key=os.environ.get("SECRET_ACCESS_KEY"))
-    s3_client = s3_session.client(service_name="s3", endpoint_url=f"http://{ip_address}:{port}")
-    s3_client.list_buckets()
-    s3_client.create_bucket(Bucket="mybucket")
-    s3_client.upload_file("test.foo1", "mybucket", "test.foo1")
+    s3_client.create_bucket(Bucket=test_bucket)
+    s3_client.upload_file("test.foo1", test_bucket, "test.foo1")
     s3_client.list_objects(Bucket='mybucket')
-
-    for file in files:
-        with s3fs.open(file, "w") as f:
-            f.write(f"test file: {file}")
+    s3fs.put_file("test.foo2", f"s3://{test_bucket}/test.foo2")
 
     s3fs.ls(f"{test_bucket}")
-
-def test_load_all_files(init_s3_files):
-    print('asdf')
 
 # @mock_aws
 # @pytest.mark.skip(reason="not working currently, not intended to use this module anyway")
-def test_s3fs_with_moto():
-    # server = ThreadedMotoServer(ip_address="127.0.0.1", port=5000)
-    server = ThreadedMotoServer(port=0)
-    server.start()
-    host, port = server.get_host_and_port()
-
-    with open("test.foo1", "w") as file:
-        file.write("test123")
-
-    with open("test.bar", "w") as file:
-        file.write("test456")
-
-    # Create a mock S3 bucket
-    #s3 = boto3.client("s3", region_name='us-east-1', endpoint_url="http://localhost:5000")
-    s3_session = boto3.Session() # aws_access_key_id=os.environ.get("ACCESS_KEY_ID"), aws_secret_access_key=os.environ.get("SECRET_ACCESS_KEY"))
-    s3_client = s3_session.client(service_name="s3", endpoint_url=f"http://{host}:{port}")
-    sts_client = s3_session.client(service_name="sts", endpoint_url=f"http://{host}:{port}")
-    session_token = sts_client.get_session_token()
-    s3_client.list_buckets()
-    s3_client.create_bucket(Bucket="mybucket")
-    s3_client.upload_file("test.foo1", "mybucket", "test.foo1")
-    s3_client.list_objects(Bucket='mybucket')
-
-    # s3fs = S3FileSystem(session=s3_session, endpoint_url="http://localhost:5000")
-    s3fs2 = S3FileSystem(token=session_token, use_ssl=False, endpoint_url=f"http://{host}:{port}")
-    s3fs2.ls("s3://mybucket")
-    # Read the file from the mock bucket
-    with s3fs2.open("s3://mybucket/test.foo1", "r") as f:
-        content = f.read()
-
-    # s3fs._copy("mybucket/test.foo", "mybucket/test.foo123")
-    s3fs2.put("test.bar", f"s3://mybucket/test.bar")
-    s3fs2.ls('s3://mybucket')
-    # fs.put("test.txt", "mybucket/test.txt")
-    assert content == "test.txt"
+# def test_s3fs_with_moto():
+#     # server = ThreadedMotoServer(ip_address="127.0.0.1", port=5000)
+#     print('asdf')
+    # server = ThreadedMotoServer(port=0)
+    # server.start()
+    # host, port = server.get_host_and_port()
+    #
+    # with open("test.foo1", "w") as file:
+    #     file.write("test123")
+    #
+    # with open("test.bar", "w") as file:
+    #     file.write("test456")
+    #
+    # # Create a mock S3 bucket
+    # #s3 = boto3.client("s3", region_name='us-east-1', endpoint_url="http://localhost:5000")
+    # s3_session = boto3.Session() # aws_access_key_id=os.environ.get("ACCESS_KEY_ID"), aws_secret_access_key=os.environ.get("SECRET_ACCESS_KEY"))
+    # s3_client = s3_session.client(service_name="s3", endpoint_url=f"http://{host}:{port}")
+    # sts_client = s3_session.client(service_name="sts", endpoint_url=f"http://{host}:{port}")
+    # session_token = sts_client.get_session_token()
+    # s3_client.list_buckets()
+    # s3_client.create_bucket(Bucket="mybucket")
+    # s3_client.upload_file("test.foo1", "mybucket", "test.foo1")
+    # s3_client.list_objects(Bucket='mybucket')
+    #
+    # # s3fs = S3FileSystem(session=s3_session, endpoint_url="http://localhost:5000")
+    # s3fs2 = S3FileSystem(token=session_token, use_ssl=False, endpoint_url=f"http://{host}:{port}")
+    # s3fs2.ls("s3://mybucket")
+    # # Read the file from the mock bucket
+    # with s3fs2.open("s3://mybucket/test.foo1", "r") as f:
+    #     content = f.read()
+    #
+    # # s3fs._copy("mybucket/test.foo", "mybucket/test.foo123")
+    # s3fs2.put("test.bar", f"s3://mybucket/test.bar")
+    # s3fs2.ls('s3://mybucket')
+    # # fs.put("test.txt", "mybucket/test.txt")
+    # assert content == "test.txt"
