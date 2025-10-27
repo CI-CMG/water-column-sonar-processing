2fb4d8b5ac4e72cb796706336e48aa6be3f13d62
oftfrfbf <rudyklucik@gmail.com>
2025-10-27 16:41:44 -0600

    trying again

--- water_column_sonar_processing/processing/__init__.py
+++ water_column_sonar_processing/processing/__init__.py
@@ -1,6 +1,4 @@
-# from .cruise_sampler import CruiseSampler
-from .batch_downloader import BatchDownloader
 from .raw_to_netcdf import RawToNetCDF
 from .raw_to_zarr import RawToZarr, get_water_level
 
-__all__ = ["RawToZarr", "get_water_level", "RawToNetCDF", "BatchDownloader"]
+__all__ = ["RawToZarr", "get_water_level", "RawToNetCDF"]
