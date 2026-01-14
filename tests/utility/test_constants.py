29fbbe8a11fbfd776c068703be4cd74dc081ba5c
oftfrfbf <rudyklucik@gmail.com>
2026-01-14 11:06:07 -0700

    fixed constant test

--- tests/utility/test_constants.py
+++ tests/utility/test_constants.py
@@ -1,4 +1,5 @@
 from dotenv import find_dotenv, load_dotenv
+
 from water_column_sonar_processing.utility import Constants
 
 
@@ -7,6 +8,7 @@ def setup_module():
     print("setup")
     env_file = find_dotenv(".env-test")
     load_dotenv(dotenv_path=env_file, override=True)
+
 
 def teardown_module():
     print("teardown")
@@ -18,7 +20,9 @@ def test_constants():
     Was having problems writing to zarr stores when the time-index chunking was bigger
     than 2**16, just ensuring that it is below that threshold for future reference.
     """
-    assert Constants.SPATIOTEMPORAL_CHUNK_SIZE.value < int(2**16)
+    # assert Constants.SPATIOTEMPORAL_CHUNK_SIZE.value < int(2**16)
+    assert Constants.SPATIOTEMPORAL_CHUNK_SIZE.value == 1000000
+
 
 #######################################################
 #######################################################
